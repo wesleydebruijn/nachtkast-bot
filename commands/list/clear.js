66bf57c76b9c0a67fbb4f654d561daef53a79c5d
Jeffrey van Hoven <wolferine@live.se>
2016-04-10 00:30:09 +0200

    Added clear command

---
+++ commands/list/clear.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let data = require('./../data');
+let util = require('./../util');
+
+var info = module.exports = {
+    identifier: "clear",
+    invoke: function() {
+        let bot = arguments[0];
+        let message = arguments[1];
+        let textChannel = arguments[2][1];
+        let limit = arguments[2][2];
+
+        if(limit > 50) {
+            limit = 50;
+        }
+
+        if(textChannel != null) {
+            var channel = bot.channels.get("name", textChannel);
+            if(channel.type != "voice") {
+                bot.getChannelLogs(channel, limit, {}, function(err, messages) {
+                    if(err) console.log(err);
+                    for(var key in messages) {
+                        var msg = messages[key];
+                        bot.deleteMessage(msg, function(err) {
+                            if(err) console.log(err);
+                        });
+                    }
+                });
+            }
+        }
+
+        bot.sendMessage(message.channel, message.author.username + " cleared " + limit + " messages in " + textChannel + ".", { tts: false }, function(err, message) {
+            if(err) console.log(err);
+        });
+    }
+}
